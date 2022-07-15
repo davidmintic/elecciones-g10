2749d3724c8c7b8b94d3ec4f4597f6b01acec481
davidmintic <90147680+davidmintic@users.noreply.github.com>
2022-07-15 08:35:59 -0500

    carga inicial del proyecto

---
+++ src/main/java/Clases/ClsCandidato.java
@@ -0,0 +1,63 @@
+package Clases;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author davidperezarias
+ */
+public class ClsCandidato extends ClsPersona {
+
+    private String partidoPolitico;
+    private String descripcion;
+    private String mensajeCampania;
+    private String ciudadorigen;
+    private LinkedList<String> propuestas;
+
+    public ClsCandidato(String partidoPolitico, String descripcion, String numeroDocumento, String nombre, String telefono, String correo) {
+        super(numeroDocumento, nombre, telefono, correo);
+        this.partidoPolitico = partidoPolitico;
+        this.descripcion = descripcion;
+    }
+
+    public String getPartidoPolitico() {
+        return partidoPolitico;
+    }
+
+    public void setPartidoPolitico(String partidoPolitico) {
+        this.partidoPolitico = partidoPolitico;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getMensajeCampania() {
+        return mensajeCampania;
+    }
+
+    public void setMensajeCampania(String mensajeCampania) {
+        this.mensajeCampania = mensajeCampania;
+    }
+
+    public String getCiudadorigen() {
+        return ciudadorigen;
+    }
+
+    public void setCiudadorigen(String ciudadorigen) {
+        this.ciudadorigen = ciudadorigen;
+    }
+
+    public LinkedList<String> getPropuestas() {
+        return propuestas;
+    }
+
+    public void setPropuestas(LinkedList<String> propuestas) {
+        this.propuestas = propuestas;
+    }
+
+}
